70206bacf88d7daefe81efd9ee162d3c0e3199b9
Wouter Dijk <wtrdk@wtrdks-MBP.local>
2020-09-14 15:21:18 +0200

    Fix

--- netcheck.sh
+++ netcheck.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 global_log_file="$HOME/Dropbox/Logs/Netcheck/netcheck_global_$(date +%Y-%m-%d).log"
-format_date() {
-  date "+%Y-%m-%d %H:%M:%S" "$1"
-}
+nice_date="+%Y-%m-%d %H:%M:%S"
 
 if [ ! -f "$global_log_file" ]; then
     touch "$global_log_file"
@@ -11,7 +9,7 @@ fi
 
 wget -q --tries=10 --timeout=20 --spider http://google.com
 if [[ $? -eq 0 ]]; then
-        echo "$(format_date)    Online" >> "$global_log_file"
+        echo "$(date "$nice_date")    Online"
 else
-        echo "$(format_date)    Offline" >> "$global_log_file"
+        echo "$(date "$nice_date")    Offline" >> "$global_log_file"
 fi
